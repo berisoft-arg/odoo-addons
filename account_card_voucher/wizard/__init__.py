59219bbe5abc038a9ac16493c554a8959fdd8b86
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2023-03-27 16:42:31 -0300

    Delete account_card_voucher directory

--- account_card_voucher/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_voucher_action_wizard
-from . import print_pre_numbered_vouchers
-from . import res_config_settings
