81b6109dffdf5dc434e1af5ab514f14dca19d629
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2023-11-11 23:55:40 -0300

    Delete account_voucher_credit_card directory

--- account_voucher_credit_card/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_payment_method
-from . import account_voucher_credit_card
-from . import account_payment
-from . import account_journal
-from . import installments_card
-from . import res_card
