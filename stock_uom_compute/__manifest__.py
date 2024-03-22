443633d2fec19a76d9cf5f60f0545bd261fc530a
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2024-03-22 18:38:33 -0300

    Update __manifest__.py

--- stock_uom_compute/__manifest__.py
+++ stock_uom_compute/__manifest__.py
@@ -14,19 +14,16 @@
 
     'author': "Sergio Ariel Ameghino",
     'website': "ariel.ameghino@gmail.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Inventory',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'stock'],
 
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/stock_valuation_layer.xml',
             ],
+
+    'installable': True,
+    
    }
