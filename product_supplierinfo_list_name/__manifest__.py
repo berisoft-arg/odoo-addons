767040d24a1f25b855ef6dea52d050be78e689f6
Sergio Ariel Ameghino <ariel.ameghino@gmail.com>
2024-03-22 17:20:29 -0300

    Update __manifest__.py

--- product_supplierinfo_list_name/__manifest__.py
+++ product_supplierinfo_list_name/__manifest__.py
@@ -12,21 +12,13 @@
 
     'author': "Sergio Ariel Ameghino",
     'website': "ariel.ameghino@gmail.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Purchase',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '15.0',
     'depends': ['product'],
-
-    # always loaded
+    
     'data': [
         # 'security/ir.model.access.csv',
         'views/product_supplierinfo_view.xml',
     ],
-    # only loaded in demonstration mode
     
 }
